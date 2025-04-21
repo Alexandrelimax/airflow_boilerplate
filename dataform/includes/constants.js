ebb3f9e16f69e1eaee5b980f4312edd421c4d7c4
Alexandre <alexandrelimaxxxx@gmail.com>
2025-04-21 18:05:36 -0300

    feature: new Implementations on dataform

---
+++ dataform/includes/constants.js
@@ -0,0 +1,6 @@
+module.exports = {
+    validOrderStatus: ['completed', 'cancelled', 'pending'],
+    silverSchema: 'silver_ecommerce',
+    goldSchema: 'gold_ecommerce',
+    ecommerceTags: ['ecommerce', 'prod'],
+};
