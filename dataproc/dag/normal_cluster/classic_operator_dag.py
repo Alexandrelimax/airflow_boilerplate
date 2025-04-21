3c95f88b186ceb845da4ee284c01573178ebcb87
Alexandre <alexandrelimaxxxx@gmail.com>
2025-04-21 16:01:34 -0300

    feature: Add new implementations Dataproc

--- dataproc/dag/normal_cluster/classic_operator_dag.py
+++ dataproc/dag/normal_cluster/classic_operator_dag.py
@@ -2,6 +2,31 @@ from airflow.decorators import dag
 from airflow.utils.dates import days_ago
 from dataproc_taskgroup import DataprocJobTaskGroup
 
+
+project_id = "seu-projeto-id"
+region = "us-central1"
+cluster_name = "meu-cluster-temp"
+
+cluster_config = {
+    "master_config": {
+        "num_instances": 1,
+        "machine_type_uri": "n1-standard-2",
+    },
+    "worker_config": {
+        "num_instances": 2,
+        "machine_type_uri": "n1-standard-2",
+    },
+}
+
+job_config = {
+    "placement": {"cluster_name": cluster_name},
+    "pyspark_job": {
+        "main_python_file_uri": "gs://meu-bucket/pyspark/meu_script.py",
+    },
+}
+
+
+
 @dag(
     dag_id="dataproc_taskgroup_dag",
     start_date=days_ago(1),
@@ -10,28 +35,6 @@ from dataproc_taskgroup import DataprocJobTaskGroup
     tags=["dataproc", "taskgroup"],
 )
 def dataproc_dag():
-
-    project_id = "seu-projeto-id"
-    region = "us-central1"
-    cluster_name = "meu-cluster-temp"
-
-    cluster_config = {
-        "master_config": {
-            "num_instances": 1,
-            "machine_type_uri": "n1-standard-2",
-        },
-        "worker_config": {
-            "num_instances": 2,
-            "machine_type_uri": "n1-standard-2",
-        },
-    }
-
-    job_config = {
-        "placement": {"cluster_name": cluster_name},
-        "pyspark_job": {
-            "main_python_file_uri": "gs://meu-bucket/pyspark/meu_script.py",
-        },
-    }
 
     DataprocJobTaskGroup(
         group_id="dataproc_pipeline",
--- dataproc/dag/normal_cluster/modern_taskflow_dag.py
+++ dataproc/dag/normal_cluster/modern_taskflow_dag.py
@@ -2,6 +2,21 @@ from airflow.decorators import dag
 from airflow.utils.dates import days_ago
 from dataproc_hook_taskgroup import DataprocHookTaskGroup
 
+
+
+cluster_config={
+    "master_config": {"num_instances": 1, "machine_type_uri": "n1-standard-2"},
+    "worker_config": {"num_instances": 2, "machine_type_uri": "n1-standard-2"},
+}
+
+job_config={
+    "pyspark_job": {
+        "main_python_file_uri": "gs://meu-bucket/scripts/etl.py",
+        "args": ["--input", "gs://...", "--output", "gs://..."],
+    }
+}
+
+
 @dag(
     dag_id="dataproc_hook_dag",
     start_date=days_ago(1),
@@ -16,16 +31,8 @@ def run_dataproc():
         project_id="meu-projeto-id",
         region="us-central1",
         cluster_name="cluster-temp",
-        cluster_config={
-            "master_config": {"num_instances": 1, "machine_type_uri": "n1-standard-2"},
-            "worker_config": {"num_instances": 2, "machine_type_uri": "n1-standard-2"},
-        },
-        job_config={
-            "pyspark_job": {
-                "main_python_file_uri": "gs://meu-bucket/scripts/etl.py",
-                "args": ["--input", "gs://...", "--output", "gs://..."],
-            }
-        }
+        cluster_config=cluster_config,
+        job_config=job_config,
     )
 
 dag = run_dataproc()
